1fa8b99b744541feac5490f734e11893964ec8c6
tyldar <tyldar@users.noreply.github.com>
2019-08-30 16:23:59 +0200

    Code clean

--- server_new/dist/jobs/SyncBlocks.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SyncBlocks = () => {
-};
-//# sourceMappingURL=SyncBlocks.js.map
--- server_new/dist/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const block_1 = require("./block");
-exports.Block = block_1.default;
-//# sourceMappingURL=index.js.map
