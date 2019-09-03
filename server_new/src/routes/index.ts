ba113b443ee57f588547429faec4ef038226bb62
tyldar <tyldar@users.noreply.github.com>
2019-09-03 16:37:23 +0200

    Added route validation

--- server_new/src/routes/index.ts
+++ server_new/src/routes/index.ts
@@ -10,23 +10,64 @@ import {ValidatorAddressRoute, ValidatorsIndexRoute} from "../controllers/valida
 import {SearchRoute} from "../controllers/core";
 import {MigrationShowRoute, MigrationStoreRoute} from "../controllers/migrations";
 
+import * as Joi from 'joi';
+
 const routes: ServerRoute[] = [
     {method: 'GET', path: '/blocks', handler: BlocksIndexRoute},
     {method: 'GET', path: '/blocks/latest', handler: BlocksLatestRoute},
-    {method: 'GET', path: '/blocks/{height}', handler: BlockHeightRoute},
+    {method: 'GET', path: '/blocks/{height}', handler: BlockHeightRoute, options: {
+        validate: {
+            params: {
+                height: Joi.number().required()
+            }
+        }
+    }},
 
     {method: 'GET', path: '/transactions', handler: TransactionsIndexRoute},
-    {method: 'GET', path: '/transactions/{hash}', handler:TransactionHashRoute},
+    {method: 'GET', path: '/transactions/{hash}', handler:TransactionHashRoute, options: {
+        validate: {
+            params: {
+                hash: Joi.string().required()
+            }
+        }
+    }},
 
-    {method: 'GET', path: '/accounts/{address}', handler:AccountAddressRoute},
+    {method: 'GET', path: '/accounts/{address}', handler:AccountAddressRoute, options: {
+        validate: {
+            params: {
+                address: Joi.string().required()
+            }
+        }
+    }},
 
     {method: 'GET', path: '/validators', handler: ValidatorsIndexRoute},
-    {method: 'GET', path: '/validators/{address}', handler: ValidatorAddressRoute},
+    {method: 'GET', path: '/validators/{address}', handler: ValidatorAddressRoute, options: {
+        validate: {
+            params: {
+                address: Joi.string().required()
+            }
+        }
+    }},
 
     {method: 'POST', path: '/search', handler: SearchRoute},
 
-    {method: 'GET', path: '/migration/{reference}', handler: MigrationShowRoute},
-    {method: 'POST', path: '/migration', handler: MigrationStoreRoute}
+    {method: 'GET', path: '/migration/{reference}', handler: MigrationShowRoute, options: {
+        validate: {
+            params: {
+                reference: Joi.string().required()
+            }
+        }
+    }},
+    {method: 'POST', path: '/migration', handler: MigrationStoreRoute, options: {
+        validate: {
+            payload: {
+                address: Joi.string().required(),
+                msg: Joi.string().required(),
+                sig: Joi.string().required(),
+                version: Joi.string().required()
+            }
+        }
+    }}
 ];
 
 export default routes;
