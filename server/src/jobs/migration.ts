d9d8612727fafa6c85aac07a7454721683b0cd74
tyldar <tyldar@users.noreply.github.com>
2019-09-05 12:17:54 +0200

    Automated migration process

--- server/src/jobs/migration.ts
+++ server/src/jobs/migration.ts
@@ -1,16 +1,33 @@
 import Migration from "../models/migration";
+import SandblockChainClient from "sandblock-chain-sdk-js/dist/client";
+
+const emitNewTransfer = async (destination: string, amount: number): Promise<any> => {
+    const sbc = new SandblockChainClient();
+    if(!process.env.WALLET_MIGRATION_PRIVATE_KEY){
+        throw new Error(`Missing wallet private key in the environment`);
+    }
+    await sbc.setPrivateKey(Buffer.from(process.env.WALLET_MIGRATION_PRIVATE_KEY, 'hex'));
+
+    const payload = await sbc.transfer(destination, "sbc", amount, "Satisfaction Token Migration");
+    const tx = await sbc.dispatch(payload);
+    return tx;
+
+}
 
 export const ProcessWaitingMigration = async () => {
     const migrations = await Migration.findAll({where: {state: 'WAITING'}});
     console.log(`${migrations.length} migrations requests to process`);
-    await migrations.forEach((migration: Migration, index: number)=>{
-        //TODO: Emit transaction from wallet
-        //const tx = null;
-        const tx = {txhash: 'jefaisuntest'};
-        if(!tx){
+    await migrations.forEach(async (migration: Migration, index: number)=>{
+        const tx = await emitNewTransfer(migration.to_address, parseInt(migration.amount));
+        if(!tx || !tx.logs || tx.logs.length  <= 0 || !tx.logs[0].success){
+            migration.message = "Error while processing transaction";
+            migration.tx_hash = tx.txhash || null;
+            migration.state = "REFUSED";
+            migration.save();
             return;
         }
 
+        migration.message = 'Migration successfully applied';
         migration.tx_hash = tx.txhash;
         migration.state = "ACCEPTED";
         migration.save();
