2c1a8dbe16ac2b0e0c5432b6a75d6f2263986e34
Segfault <tyldar@users.noreply.github.com>
2020-10-15 19:01:28 +0200

    Wallet fixes

--- client/src/utils/redux.js
+++ client/src/utils/redux.js
@@ -12,7 +12,7 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 const clients = {
     api: {
         client: axios.create({
-            baseURL: `https://api.explorer.sandblock.io/api/v1`,
+            baseURL: `http://51.158.102.88:32538`,
             headers: {
                 'Content-Type': 'application/json',
                 Accept: 'application/json'
