bcf5568592764e410b151b7f2827cb3b1ceaade2
Segfault <tyldar@users.noreply.github.com>
2021-02-24 10:03:14 +0100

    Return validator if starts with the prefix

--- src/Http/Controllers/CoreController.ts
+++ src/Http/Controllers/CoreController.ts
@@ -2,14 +2,13 @@ import {
     CacheInterceptor,
     Controller,
     Get,
-    InternalServerErrorException,
     NotFoundException,
     Req,
-    UseInterceptors
-} from "@nestjs/common";
-import {Request} from "express";
-import {ElasticService} from "@app/Services";
-import {ElasticIndexes} from "@app/Utils/Constants";
+    UseInterceptors,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { ElasticService } from '@app/Services';
+import { ElasticIndexes } from '@app/Utils/Constants';
 
 @Controller('')
 export default class CoreController {
@@ -20,14 +19,16 @@ export default class CoreController {
 
         // We check the different combinations
         if (/^\d+$/.test(data)) {
-            return {type: 'block', data};
+            return { type: 'block', data };
+        } else if (String(data).startsWith('sandvaloper')) {
+            return { type: 'validator', data };
         } else if (String(data).startsWith('sand')) {
-            return {type: 'account', data};
+            return { type: 'account', data };
         } else {
             if ((await ElasticService.getInstance().documentExists(ElasticIndexes.INDEX_BLOCKS, data))) {
-                return {type: 'block', data};
+                return { type: 'block', data };
             } else if ((await ElasticService.getInstance().documentExists(ElasticIndexes.INDEX_TRANSACTIONS, data))) {
-                return {type: 'transaction', data};
+                return { type: 'transaction', data };
             } else {
                 throw new NotFoundException('data_not_found');
             }
