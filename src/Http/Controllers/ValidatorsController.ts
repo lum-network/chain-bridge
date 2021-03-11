fcbc4c4dd8737e5164e5e21551cc9c59fe1b67f5
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-03-11 16:01:29 +0100

    Revamp validators show

--- src/Http/Controllers/ValidatorsController.ts
+++ src/Http/Controllers/ValidatorsController.ts
@@ -8,42 +8,38 @@ import { ValidatorResponse } from '@app/Http/Responses';
 @Controller('validators')
 @UseInterceptors(CacheInterceptor)
 export default class ValidatorsController {
-    constructor(private readonly _lumNetworkService: LumNetworkService) {
-    }
+    constructor(private readonly _lumNetworkService: LumNetworkService) {}
 
     @Get('')
     async fetch() {
         const lumClt = await this._lumNetworkService.getClient();
-        const {validators} = lumClt.queryClient.staking.unverified;
+        const { validators } = lumClt.queryClient.staking.unverified;
 
         // We acquire both bounded and unbonded (candidates) validators
-        const [bonded, unbonding, unbonded] = await Promise.all([validators('BOND_STATUS_BONDED'), validators('BOND_STATUS_UNBONDING'), validators('BOND_STATUS_UNBONDED')])
+        const [bonded, unbonding, unbonded] = await Promise.all([validators('BOND_STATUS_BONDED'), validators('BOND_STATUS_UNBONDING'), validators('BOND_STATUS_UNBONDED')]);
 
         const results = [...bonded.validators, ...unbonding.validators, ...unbonded.validators];
 
-        return results.map(validator => plainToClass(ValidatorResponse, validator));
+        return results.map((validator) => plainToClass(ValidatorResponse, validator));
     }
 
     @Get(':address')
     async show(@Req() req: Request) {
         const lumClt = await this._lumNetworkService.getClient();
 
-        // Acquire the validator
-        const validator = await lumClt.queryClient.staking.unverified.validator(req.params.address);
+        const [validator, delegations, rewards] = await Promise.all([
+            lumClt.queryClient.staking.unverified.validator(req.params.address),
+            lumClt.queryClient.staking.unverified.validatorDelegations(req.params.address),
+            lumClt.queryClient.distribution.unverified.validatorOutstandingRewards(req.params.address),
+        ]);
 
         if (!validator) {
             throw new NotFoundException('validator_not_found');
         }
 
-        // Acquire the delegations list
-        const delegations = await lumClt.queryClient.staking.unverified.validatorDelegations(req.params.address);
-
         if (!delegations) {
             throw new NotFoundException('validator_delegations_not_found');
         }
-
-        // Acquire the rewards
-        const rewards = await lumClt.queryClient.distribution.unverified.validatorOutstandingRewards(req.params.address)
 
         if (!rewards) {
             throw new NotFoundException('validator_rewards_not_found');
@@ -54,7 +50,7 @@ export default class ValidatorsController {
             ...validator.validator,
             delegations,
             rewards,
-        }
+        };
 
         return plainToClass(ValidatorResponse, result);
     }
