413cd61966e66691506d7a2b8be434ea1facabe6
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-05-14 15:02:22 +0200

    Hotfix for faucet processing (#21)
    * Fixed faucet incorrect branch
    
    * Improvement/sbc 65 (#5)
    
    * Revamp blocks
    
    * Revamp account and blocks
    
    * Revamp validator
    
    * Merge Promise
    
    * Revamp account
    
    * Revamp validators show
    
    * Remove console.log
    
    * Transaction messages done
    
    * Add almost all messages types
    
    * Fix transactions in block
    
    * Fix Account
    
    * Remove console.log
    
    * Fix search
    
    * Return delegations and blocks
    
    * Self bonded done
    
    * Added faucet capability
    
    * Fix account
    
    * Upgrade to 0.3.0 lum-network/sdk
    
    * Added metricbeat and kibana
    
    * Added metricbeat config
    
    * Fix memo
    
    * Add transaction messages types
    
    * Add fees to transaction response
    
    * Fixed pipeline
    
    * Add unbonding
    
    * code style fixes
    
    * Get reward done
    
    * rewards details done
    
    * Unbonding done
    
    Co-authored-by: Segfault <tyldar@users.noreply.github.com>
    
    * Work-in-progress github actions split
    
    * Pipeline should pass with no test inside
    
    * Should occur on synchronize as well
    
    * Added WIP deploy testnet task
    
    * CI/CD and contributing doc
    
    * [SBC-101] Fix validator account address (#7)
    
    * Fix validator account address
    
    * Fix linter warnings
    
    * Missing implementation for faucet mnemonic + code style issues fix
    
    * Fixed ulum denom
    
    * [SBC-117] Update JS-SDK to 0.3.5 (#8)
    
    * Update js sdk
    
    * Fix amount
    
    * CD pipe using remote directory (#14)
    
    * First try for other repo clone
    
    * Upgrade for access token
    
    * Testing with other naming
    
    * Added deployment
    
    * use single path
    
    * use correct version
    
    * only deploy on master
    
    Co-authored-by: Segfault <tyldar@users.noreply.github.com>
    
    * Code style improvement
    
    * Fixed faucet amount
    
    * code style fixes
    
    * Update github deployment file
    
    * Added beam messages handling
    
    * code style fixes
    
    * Rate limit faucet queue
    
    Co-authored-by: Segfault <tyldar@users.noreply.github.com>
    Co-authored-by: Thibault Jaillard <thibault.jaillard@gmail.com>
    Co-authored-by: Enguerrand Pinot <enguerrand@baldr.co>

--- src/http/controllers/core.controller.ts
+++ src/http/controllers/core.controller.ts
@@ -7,7 +7,7 @@ import { InjectQueue } from '@nestjs/bull';
 
 @Controller('')
 export class CoreController {
-    constructor(private readonly _elasticService: ElasticService, @InjectQueue(Queues.QUEUE_DEFAULT) private readonly _queue: Queue) {}
+    constructor(private readonly _elasticService: ElasticService, @InjectQueue(Queues.QUEUE_FAUCET) private readonly _queue: Queue) {}
 
     @Get('search/:data')
     @UseInterceptors(CacheInterceptor)
