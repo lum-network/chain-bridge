4a2eff619c4709a74b9fd8ef5a41511b75b36ca6
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-10-01 12:25:20 +0200

    Add get proposal

--- src/http/controllers/governance.controller.ts
+++ src/http/controllers/governance.controller.ts
@@ -1,4 +1,4 @@
-import { CacheInterceptor, Controller, Get, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor, Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import { LumNetworkService } from '@app/services';
 import { ProposalStatus } from '@lum-network/sdk-javascript/build/codec/cosmos/gov/v1beta1/gov';
 import { plainToClass } from 'class-transformer';
@@ -26,5 +26,14 @@ export class GovernanceController {
         );
 
         return results.proposals.map(proposal => plainToClass(ProposalResponse, proposal));
+    }
+
+    @Get('proposals/:id')
+    async get(@Param('id') id: string) {
+        const lumClt = await this._lumNetworkService.getClient();
+
+        const result = await lumClt.queryClient.gov.proposal(id);
+
+        return plainToClass(ProposalResponse, result.proposal);
     }
 }
