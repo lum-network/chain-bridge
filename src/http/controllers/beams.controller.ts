a82dc8e045d601f96b6c8c8ca623da7a20addb7b
Thibault Jaillard <thibault.jaillard@gmail.com>
2022-03-08 17:50:23 +0100

    Fix warnings

--- src/http/controllers/beams.controller.ts
+++ src/http/controllers/beams.controller.ts
@@ -1,6 +1,5 @@
-import { CacheInterceptor, Controller, Get, NotFoundException, Param, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor, Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import { ElasticService, LumNetworkService } from '@app/services';
-import { ElasticIndexes } from '@app/utils';
 
 @Controller('beams')
 @UseInterceptors(CacheInterceptor)
@@ -9,20 +8,6 @@ export class BeamsController {
 
     @Get('')
     async fetch() {
-        // We get the 50 last beams stored in ES
-        // const result = await this._elasticService.documentSearch(ElasticIndexes.INDEX_BEAMS, {
-        //     size: 50,
-        //     sort: { height: 'desc' },
-        // });
-        //
-        // console.log(result);
-        //
-        // if (!result || !result.body || !result.body.hits || !result.body.hits.hits) {
-        //     throw new NotFoundException('beams_not_found');
-        // }
-        //
-        // return result.body.hits.hits.map(beam => beam._source);
-
         const lumClt = await this._lumNetworkService.getClient();
 
         return await lumClt.queryClient.beam.fetch();
