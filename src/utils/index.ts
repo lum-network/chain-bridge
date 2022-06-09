66ae6339158cda9f4ea3c07f42e0ffcb1115ac73
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-06-09 13:45:23 +0200

    Removed old indices

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,6 @@ export * from './config';
 export * from './constants';
 export * from './gov';
 export * from './http';
-export * from './indices';
 export * from './metadata';
 export * from './microservice';
 export * from './models';
