8f1ed1125095fd95f7f1256ded2b03c73b283fd3
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-06-09 13:59:15 +0200

    Fixed beams controller

--- src/http/controllers/beams.controller.ts
+++ src/http/controllers/beams.controller.ts
@@ -1,10 +1,11 @@
-import { CacheInterceptor, Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import {CacheInterceptor, Controller, Get, Param, Req, UseInterceptors} from '@nestjs/common';
 import {ApiOkResponse, ApiTags} from "@nestjs/swagger";
 
 import {plainToInstance} from "class-transformer";
 
 import { LumNetworkService } from '@app/services';
-import {BeamResponse, DataResponse} from "@app/http/responses";
+import {BeamResponse, DataResponse, DataResponseMetadata} from "@app/http/responses";
+import {ExplorerRequest} from "@app/utils";
 
 @ApiTags('beams')
 @Controller('beams')
@@ -12,10 +13,19 @@ import {BeamResponse, DataResponse} from "@app/http/responses";
 export class BeamsController {
     constructor(private readonly _lumNetworkService: LumNetworkService) {}
 
+    @ApiOkResponse({status: 200, type: [BeamResponse]})
     @Get('')
-    async fetch() {
+    async fetch(@Req() request: ExplorerRequest): Promise<DataResponse> {
         const beams = await this._lumNetworkService.client.queryClient.beam.fetch();
-        return beams;
+        return new DataResponse({
+            result: beams.map(beam => plainToInstance(BeamResponse, beam)),
+            metadata: new DataResponseMetadata({
+                page: request.pagination.page,
+                limit: request.pagination.limit,
+                items_count: beams.length,
+                items_total: null,
+            })
+        })
     }
 
     @ApiOkResponse({type: BeamResponse})
