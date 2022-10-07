ca94e46d5281b34308023b1b940cee8d7313d89f
Ricardo-Remy <ricardoremy@Ricardos-MacBook-Pro.local>
2022-10-07 18:12:46 +0100

    feat: create functions to sync voters and depositors

--- src/async/schedulers/governance.scheduler.ts
+++ src/async/schedulers/governance.scheduler.ts
@@ -13,11 +13,11 @@ export class GovernanceScheduler {
     constructor(private readonly _lumNetworkService: LumNetworkService) {}
 
     @Cron(CronExpression.EVERY_10_SECONDS)
-    async voteSync() {
+    async proposalsIdSync(): Promise<number[]> {
         try {
             this._logger.log(`Syncing proposals from chain...`);
-            // We want to get the proposals in order to fetch the id
-            // The id will be used to access who voted on the proposal and the related deposited amount
+
+            // We want to sync all proposals and get the proposal_id
             const resultsProposals = await this._lumNetworkService.client.queryClient.gov.proposals(
                 ProposalStatus.PROPOSAL_STATUS_UNSPECIFIED |
                     ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD |
@@ -30,21 +30,56 @@ export class GovernanceScheduler {
                 '',
             );
 
-            // We iterate throught the proposals to store the Ids
-            const getProposalId = resultsProposals.proposals.map((proposal) => proposal.proposalId).map((proposalIdValue) => proposalIdValue.low);
+            // Map the results proposal to get the low int from Long
+            const getProposalId = resultsProposals?.proposals.map((proposal) => proposal.proposalId).map((longInt) => longInt.low);
 
-            // We get the votes of the proposal
-            for (const id of getProposalId) {
-                const getVotesById = await this._lumNetworkService.client.queryClient.gov.votes(id);
-                // Todo: save in DB
-                this._logger.log(`Found getVotesById`, getVotesById);
+            this._logger.log(`Found ${getProposalId.length} proposalId - ${getProposalId}`);
+            // return the getProposalId
+            return getProposalId;
+        } catch (error) {
+            this._logger.error(`Failed to sync proposals from chain...`, error);
+        }
+    }
 
-                // Todo: Index
+    @Cron(CronExpression.EVERY_10_SECONDS)
+    async voteSync() {
+        try {
+            this._logger.log(`Syncing votes from chain...`);
 
-                // If we have pagination key, we just patch it and it will process in the next loop
+            // We need to get the proposalsId in order to fetch the voters
+            const getVotersByProposalId = await this.proposalsIdSync();
+
+            for (const id of getVotersByProposalId) {
+                // Fetch the votes based on the proposalId
+                const getVotes = await this._lumNetworkService.client.queryClient.gov.votes(id);
+                // Map the votes to get the voters
+                const getVoter = getVotes.votes.map((voterHash) => voterHash.voter);
+                this._logger.log(`Found ${getVoter.length} - Voters - ${getVoter}`);
             }
         } catch (error) {
-            this._logger.error(`Failed to fetch proposals from chain`, error);
+            this._logger.error(`Failed to sync votes from chain...`, error);
+        }
+    }
+
+    @Cron(CronExpression.EVERY_10_SECONDS)
+    async depositSync() {
+        try {
+            this._logger.log(`Syncing deposits from chain...`);
+
+            // We need to get the proposalsId in order to fetch the deposits
+            const getDepositsByProposalId = await this.proposalsIdSync();
+
+            for (const id of getDepositsByProposalId) {
+                // Fetch the deposits based on the proposalId
+                const getDeposits = await this._lumNetworkService.client.queryClient.gov.deposits(id);
+
+                // Map the deposits to get the depositors
+                const getDepositor = getDeposits.deposits.map((depositorHash) => depositorHash.depositor);
+
+                this._logger.log(`Found ${getDepositor.length} - Depositors - ${getDepositor}`);
+            }
+        } catch (error) {
+            this._logger.error(`Failed to sync deposits from chain...`, error);
         }
     }
 }
