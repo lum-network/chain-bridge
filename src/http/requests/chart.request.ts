5343f0818078cfcce54e03e4a59486d768b4cdc4
Ricardo-Remy <rcd.remy@gmail.com>
2022-11-07 10:44:27 +0000

    feat: pass groupType as optional

--- src/http/requests/chart.request.ts
+++ src/http/requests/chart.request.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsDateString, IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { IsDateString, IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { ChartGroupType, ChartTypes } from '@app/utils';
 
 export class ChartRequest {
@@ -20,6 +20,7 @@ export class ChartRequest {
     type: ChartTypes;
 
     @ApiProperty()
+    @IsOptional()
     @IsNotEmpty()
     @IsString()
     @IsIn([ChartGroupType.GROUP_DAILY, ChartGroupType.GROUP_MONTHLY, ChartGroupType.GROUP_YEARLY])
