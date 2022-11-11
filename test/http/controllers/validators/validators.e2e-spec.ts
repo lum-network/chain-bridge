a53be109053dc040ecc937055114776e18ab5270
Ricardo-Remy <rcd.remy@gmail.com>
2022-11-11 11:44:44 +0000

    feat: compute self bonded for each validator

---
+++ test/http/controllers/validators/validators.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { INestApplication } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiModule } from '@app/modules';
+import request from 'supertest';
+
+import { mockAddressValidatorResponse, mockValidatorBlockResponse, mockValidatorDelegationsResponse, mockValidatorsResponse } from './mock.data';
+
+import { DatabaseConfig } from '@app/database';
+
+describe('Validators (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [
+                ApiModule,
+
+                // Use the e2e_test database to run the tests
+
+                TypeOrmModule.forRootAsync(DatabaseConfig),
+            ],
+        }).compile();
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it('[GET] - should return a list of validators', () => {
+        return request(app.getHttpServer()).get('/validators').expect(mockValidatorsResponse);
+    });
+
+    it('[GET] - should return information on a specific validator (Lum Foundation)', () => {
+        return request(app.getHttpServer()).get('/validators/lumvaloper1qx2dts3tglxcu0jh47k7ghstsn4nactufgmmlk').expect(mockAddressValidatorResponse);
+    });
+
+    it('[GET] - should return information on blocks for a specific validator (Lum Foundation)', () => {
+        return request(app.getHttpServer()).get('/validators/lumvaloper1qx2dts3tglxcu0jh47k7ghstsn4nactufgmmlk/blocks').expect(mockValidatorBlockResponse);
+    });
+
+    it('[GET] - should return information on delegations for a specific validator (Lum Foundation)', () => {
+        return request(app.getHttpServer()).get('/validators/lumvaloper1qx2dts3tglxcu0jh47k7ghstsn4nactufgmmlk/delegations').expect(mockValidatorDelegationsResponse);
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+});
