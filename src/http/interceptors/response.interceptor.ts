b110142b8b058274a182ff8a4fe93257ea58ce48
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-01-18 08:57:50 +0100

    Fixed response interceptor

--- src/http/interceptors/response.interceptor.ts
+++ src/http/interceptors/response.interceptor.ts
@@ -1,8 +1,9 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {Injectable, NestInterceptor, ExecutionContext, CallHandler} from '@nestjs/common';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import {map} from 'rxjs/operators';
+import {Request} from "express";
 
 export interface Response<T> {
     code: number;
@@ -13,6 +14,13 @@ export interface Response<T> {
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
     intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+        // We don't want to override the response if it's a metrics prometheus call
+        const request = context.switchToHttp().getRequest<Request>();
+        if (request.url === "/metrics") {
+            return next.handle();
+        }
+
+        // Otherwise we remap the response
         return next.handle().pipe(
             map((data) => ({
                 ...data,
