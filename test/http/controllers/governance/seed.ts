99a706ce94d868546c471cac4230bc71beea25c1
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-01-19 18:57:38 +0100

    Make sure id 1 does exists

--- test/http/controllers/governance/seed.ts
+++ test/http/controllers/governance/seed.ts
@@ -1,11 +1,11 @@
 export const depositSeed = {
-    proposalId: 10,
+    proposalId: 1,
     depositorAddress: 'lumvaloper1ss7vkqzfal7h4m8ug8lh8peqqyt7vq3sjr9zvg',
     amount: { denom: 'ulum', amount: '100000000000' },
 };
 
 export const voteSeed = {
-    proposalId: 10,
+    proposalId: 1,
     voterAddress: 'lum1ugeeckr6frejr62jm60m69zxy7ttudc3w584kn',
     voteOption: 2,
     voteWeight: '1000000000000000000',
