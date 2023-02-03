e0f316498c92633d7289621f4e509c1911be3961
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-02-03 11:36:53 +0100

    Improved code stability

--- src/async/schedulers/governance.scheduler.ts
+++ src/async/schedulers/governance.scheduler.ts
@@ -31,14 +31,16 @@ export class GovernanceScheduler {
         const proposals = await this._chainService.getChain<LumChain>(AssetSymbol.LUM).getProposals();
         for (const proposal of proposals.proposals) {
             // Try to decode the main message
-            const decodedMsg = LumRegistry.decode(proposal.messages[0]);
             let decodedContent = null;
-            if (decodedMsg.content) {
-                decodedContent = LumRegistry.decode(decodedMsg.content);
+            if (proposal.messages.length > 0) {
+                const decodedMsg = LumRegistry.decode(proposal.messages[0]);
+                if (decodedMsg.content) {
+                    decodedContent = LumRegistry.decode(decodedMsg.content);
 
-                // If the decoded content is a MsgSubmitProposal it contains plan, we need to patch the height value to store raw int
-                if (decodedContent.plan !== undefined && decodedContent.plan !== null) {
-                    decodedContent.plan.height = decodedContent.plan.height.toNumber();
+                    // If the decoded content is a MsgSubmitProposal it contains plan, we need to patch the height value to store raw int
+                    if (decodedContent.plan !== undefined && decodedContent.plan !== null) {
+                        decodedContent.plan.height = decodedContent.plan.height.toNumber();
+                    }
                 }
             }
 
@@ -60,6 +62,7 @@ export class GovernanceScheduler {
                 voting_start_time: proposal.votingStartTime,
                 voting_end_time: proposal.votingEndTime,
             });
+            this._logger.debug(`Synced proposal #${proposal.id.toNumber()}`);
         }
     }
 
@@ -68,41 +71,37 @@ export class GovernanceScheduler {
         if (!this._configService.get<boolean>('GOVERNANCE_SYNC_ENABLED')) {
             return;
         }
-        try {
-            this._logger.log(`Syncing votes from chain...`);
+        this._logger.log(`Syncing votes from chain...`);
 
-            // We need to get the proposalsId in order to fetch the voters
-            const proposalIds = await this._chainService.getChain<LumChain>(AssetSymbol.LUM).getOpenVotingProposals();
+        // We need to get the proposalsId in order to fetch the voters
+        const proposalIds = await this._chainService.getChain<LumChain>(AssetSymbol.LUM).getOpenVotingProposals();
 
-            if (!proposalIds || !proposalIds.length) {
-                this._logger.log(`voteSync scheduler not launched as no current open proposals to vote on...`);
-                return;
-            }
+        if (!proposalIds || !proposalIds.length) {
+            this._logger.log(`voteSync scheduler not launched as no current open proposals to vote on...`);
+            return;
+        }
 
-            // Only start the patch process if there are actual proposalId
-            for (const id of proposalIds) {
-                // Fetch the votes based on the proposalId
-                const getVotes = await this._chainService.getChain(AssetSymbol.LUM).client.queryClient.gov.votes(id);
+        // Only start the patch process if there are actual proposalId
+        for (const id of proposalIds) {
+            // Fetch the votes based on the proposalId
+            const getVotes = await this._chainService.getChain(AssetSymbol.LUM).client.queryClient.gov.votes(id);
 
-                // Map the votes to get the voters, the voteOption and the voteWeight
-                const getVoterAndOptions = getVotes.votes.map((voteArgs) => ({
-                    voter: voteArgs.voter,
-                    voteOption: voteArgs.options[0].option,
-                    voteWeight: voteArgs.options[0].weight,
-                }));
+            // Map the votes to get the voters, the voteOption and the voteWeight
+            const getVoterAndOptions = getVotes.votes.map((voteArgs) => ({
+                voter: voteArgs.voter,
+                voteOption: voteArgs.options[0].option,
+                voteWeight: voteArgs.options[0].weight,
+            }));
 
-                // Create or update to DB if we have new voters based on the proposalId
-                for (const voteKey of getVoterAndOptions) {
-                    // Only update the db if there is any vote during the voting period
-                    if (getVotes && getVotes.votes && getVotes.votes.length > 0) {
-                        await this._governanceProposalVoteService.createOrUpdateVoters(id, voteKey.voter, voteKey.voteOption, voteKey.voteWeight);
-                    }
+            // Create or update to DB if we have new voters based on the proposalId
+            for (const voteKey of getVoterAndOptions) {
+                // Only update the db if there is any vote during the voting period
+                if (getVotes && getVotes.votes && getVotes.votes.length > 0) {
+                    await this._governanceProposalVoteService.createOrUpdateVoters(id, voteKey.voter, voteKey.voteOption, voteKey.voteWeight);
                 }
             }
-            this._logger.log(`Synced ${proposalIds.length || 0} proposals from chain...`);
-        } catch (error) {
-            this._logger.error(`Failed to sync votes from chain...`, error);
         }
+        this._logger.log(`Synced ${proposalIds.length || 0} proposals from chain...`);
     }
 
     @Cron(CronExpression.EVERY_30_SECONDS)
@@ -110,36 +109,32 @@ export class GovernanceScheduler {
         if (!this._configService.get<boolean>('GOVERNANCE_SYNC_ENABLED')) {
             return;
         }
-        try {
-            this._logger.log(`Syncing deposits from chain...`);
+        this._logger.log(`Syncing deposits from chain...`);
 
-            // We need to get the proposalsId in order to fetch the deposits
-            const proposalIds = await this._chainService.getChain<LumChain>(AssetSymbol.LUM).getOpenVotingProposals();
+        // We need to get the proposalsId in order to fetch the deposits
+        const proposalIds = await this._chainService.getChain<LumChain>(AssetSymbol.LUM).getOpenVotingProposals();
 
-            if (!proposalIds || !proposalIds.length) {
-                this._logger.log(`No active proposals to sync deposits for...`);
-                return;
-            }
-
-            // Only start the patch process if there are actual proposalId
-            for (const id of proposalIds) {
-                // Fetch the deposits based on the proposalId
-                const getDeposits = await this._chainService.getChain(AssetSymbol.LUM).client.queryClient.gov.deposits(id);
-
-                // Map the deposits to get the depositors and the amount
-                const getDepositor = getDeposits.deposits.map((deposit) => ({
-                    depositor: deposit.depositor,
-                    amount: deposit.amount[0],
-                }));
-
-                // Create or update to DB if we have new depositors based on the proposalId
-                for (const depositorAddress of getDepositor) {
-                    await this._governanceProposalDepositService.createOrUpdateDepositors(id, depositorAddress.depositor, depositorAddress.amount);
-                }
-            }
-            this._logger.log(`Synced ${proposalIds.length || 0} proposals from chain...`);
-        } catch (error) {
-            this._logger.error(`Failed to sync deposits from chain...`, error);
+        if (!proposalIds || !proposalIds.length) {
+            this._logger.log(`No active proposals to sync deposits for...`);
+            return;
         }
+
+        // Only start the patch process if there are actual proposalId
+        for (const id of proposalIds) {
+            // Fetch the deposits based on the proposalId
+            const getDeposits = await this._chainService.getChain(AssetSymbol.LUM).client.queryClient.gov.deposits(id);
+
+            // Map the deposits to get the depositors and the amount
+            const getDepositor = getDeposits.deposits.map((deposit) => ({
+                depositor: deposit.depositor,
+                amount: deposit.amount[0],
+            }));
+
+            // Create or update to DB if we have new depositors based on the proposalId
+            for (const depositorAddress of getDepositor) {
+                await this._governanceProposalDepositService.createOrUpdateDepositors(id, depositorAddress.depositor, depositorAddress.amount);
+            }
+        }
+        this._logger.log(`Synced ${proposalIds.length || 0} proposals from chain...`);
     }
 }
