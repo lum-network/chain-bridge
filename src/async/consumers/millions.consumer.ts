392a626d545c0ab0b10d645ee2debcea577e2276
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-02-21 17:11:27 +0100

    Fix deposit processing

--- src/async/consumers/millions.consumer.ts
+++ src/async/consumers/millions.consumer.ts
@@ -37,7 +37,30 @@ export class MillionsConsumer {
         }
 
         let deposit = await this._millionsDepositService.getById(job.data.depositId);
-        if (!deposit && liveDeposit) {
+        if (deposit) {
+            if (deposit.block_height > job.data.height) {
+                this._logger.debug(`Millions deposit ${job.data.depositId} already ingested`);
+                return;
+            }
+            if (liveDeposit) {
+                if (deposit.is_sponsor !== liveDeposit.deposit.isSponsor) {
+                    deposit.is_sponsor = liveDeposit.deposit.isSponsor;
+                }
+                if (deposit.winner_address !== liveDeposit.deposit.winnerAddress) {
+                    deposit.winner_address = liveDeposit.deposit.winnerAddress;
+                }
+                if (deposit.depositor_address !== liveDeposit.deposit.depositorAddress) {
+                    deposit.depositor_address = liveDeposit.deposit.depositorAddress;
+                }
+            }
+            if (job.data.withdrawalId) {
+                deposit.withdrawal_id = job.data.withdrawalId;
+            }
+            await this._millionsDepositService.save(deposit);
+        } else {
+            if (!liveDeposit) {
+                throw new Error(`failed_to_create_deposit_null_live_deposit`);
+            }
             deposit = new MillionsDepositEntity({
                 id: job.data.depositId,
                 pool_id: job.data.poolId,
@@ -51,27 +74,9 @@ export class MillionsConsumer {
                 is_sponsor: liveDeposit.deposit.isSponsor,
                 withdrawal_id: job.data.withdrawalId || 0,
             });
-        } else if (deposit && liveDeposit) {
-            if (deposit.block_height > job.data.height) {
-                this._logger.debug(`Millions deposit ${job.data.depositId} already ingested`);
-                return;
-            }
-            if (deposit.is_sponsor !== liveDeposit.deposit.isSponsor) {
-                deposit.is_sponsor = liveDeposit.deposit.isSponsor;
-            }
-            if (deposit.winner_address !== liveDeposit.deposit.winnerAddress) {
-                deposit.winner_address = liveDeposit.deposit.winnerAddress;
-            }
-            if (deposit.depositor_address !== liveDeposit.deposit.depositorAddress) {
-                deposit.depositor_address = liveDeposit.deposit.depositorAddress;
-            }
+            await this._millionsDepositService.save(deposit);
         }
 
-        if (job.data.withdrawalId && deposit.withdrawal_id !== job.data.withdrawalId) {
-            deposit.withdrawal_id = job.data.withdrawalId;
-        }
-
-        await this._millionsDepositService.save(deposit);
         await job.log(`Millions deposit ${job.data.depositId} ingested`);
     }
 }
