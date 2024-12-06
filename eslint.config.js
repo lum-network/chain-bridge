f06bcd9007e77a75f1dd327ced43666572fb6b89
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-12-06 10:45:47 +0100

    Fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,11 @@ module.exports = [
             '@typescript-eslint/explicit-function-return-type': 'off',
             '@typescript-eslint/explicit-module-boundary-types': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-expressions': ['error', {
+                allowShortCircuit: true,
+                allowTernary: true,
+                allowTaggedTemplates: true
+            }],
         },
     },
     {
